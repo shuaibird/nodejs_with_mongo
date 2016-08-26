92cb2f4e92835ef9a606418e7eef9f0e0e8e62f0
Shuaibird <shuaibird@icloud.com>
2016-08-26 18:34:05 +0800

    complete admin/users.js

--- admin/users.js
+++ admin/users.js
@@ -1,17 +1,26 @@
-var uuid = require("node-uuid")
-var _ = require("lodash")
 var express = require("express")
-var users = require("./../data/users.json")
 var os = require('os')
-
-
 var {connectMongoose} = require('../data/chatDB')
-
-
 var User = require('./userModel')
 
 var router = express.Router()
 module.exports = router
+
+
+function userFromRequestBody(user, request) {
+  user.alias = request.body.alias
+  user.roles = request.body.roles
+  user.contact = {
+    phone: request.body["contact.phone"],
+    email: request.body["contact.email"]
+  }
+  user.address = {
+    lines: request.body["address.lines"].split(os.EOL),
+    city: request.body["address.city"],
+    state: request.body["address.state"],
+    zip: request.body["address.zip"]
+  }
+}
 
 
 router.get('/', (req, res) => {
@@ -27,64 +36,39 @@ router.get('/', (req, res) => {
 
 
 router.route('/add')
-  .get(function (req, res) {
-    res.render("users/add");
+  .get((req, res) => res.render("users/add"))
+  .post((req, res) => {
+    var user = new User()
+    userFromRequestBody(user, req)
+    user.save()
+      .then(() => res.redirect(req.baseUrl))
   })
-  .post(function (req, res) {
-    var user = {
-      id: uuid.v4()
-    };
 
-    userFromRequestBody(user, req);
-
-    users.push(user);
-
-    res.redirect(req.baseUrl);
-  });
-
-function userFromRequestBody(user, request) {
-  user.alias = request.body.alias;
-  user.roles = request.body.roles;
-  user.contact = {
-    phone: request.body["contact.phone"],
-    email: request.body["contact.email"]
-  };
-  user.address = {
-    lines: request.body["address.lines"].split(os.EOL),
-    city: request.body["address.city"],
-    state: request.body["address.state"],
-    zip: request.body["address.zip"]
-  };
-}
 
 router.route('/edit/:id')
-  .all(function (req, res, next) {
-    var userId = req.params.id;
-
-    var user = _.find(users, r => r.id === userId);
-    if (!user) {
-      res.sendStatus(404);
-      return;
-    }
-    res.locals.user = user;
-    res.locals.userHasRole = function (role) {
-      return (user.roles || []).indexOf(role) > -1
-    };
-    next()
+  .all((req, res, next) => {
+    var userId = req.params.id
+    User.findById(userId).exec()
+      .then(user => {
+        if (!user) {
+          res.sendStatus(404)
+          return
+        }
+        res.locals.user = user
+        res.locals.userHasRole = role => (user.roles || []).indexOf(role) > -1
+        next()
+      })
   })
-  .get(function (req, res) {
-    res.render("users/edit");
+  .get((req, res) => res.render("users/edit"))
+  .post((req, res) => {
+    userFromRequestBody(res.locals.user, req)
+    res.locals.user.save()
+      .then(() => res.redirect(req.baseUrl))
   })
-  .post(function (req, res) {
-    userFromRequestBody(res.locals.user, req);
 
-    res.redirect(req.baseUrl);
-  });
 
-router.get('/delete/:id', function (req, res) {
-  var userId = req.params.id;
-
-  users = users.filter(r => r.id !== userId);
-
-  res.redirect(req.baseUrl);
-});
+router.get('/delete/:id', (req, res) => {
+  var userId = req.params.id
+  User.remove({_id: userId})
+    .then(() => res.redirect(req.baseUrl))
+})
